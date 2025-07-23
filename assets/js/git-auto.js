b36571bc6e803272685f378538a1a03d5f8a6c29
laucha <mlbuson@teco.com.ar>
2025-07-23 01:04:11 -0300

    fix18.27

--- assets/js/git-auto.js
+++ assets/js/git-auto.js
@@ -1,1 +1,3 @@
-// JS para guardar automáticamente cambios
+// Este archivo puede estar vacío o contener funcionalidades específicas
+// si necesitas integrar con Git en el futuro
+console.log("Git auto script loaded");
